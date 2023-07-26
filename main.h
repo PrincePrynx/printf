8dd32fd07521064be3a908a81c2c921179aac1ec
PrincePrynx <poetykprynx@gmail.com>
2023-07-25 18:20:16 -0700

    PRINTF

--- main.h
+++ main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFF_SIZE 1024
-
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+
+#define BUFF_SIZE 1024
+
 
 /*Main printf function*/
 int _printf(const char *format, ...);
