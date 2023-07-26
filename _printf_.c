1031c707b50328acdc00439fd1bde772d10402e9
PrincePrynx <poetykprynx@gmail.com>
2023-07-25 17:51:01 -0700

    PRINTF

--- _printf_.c
+++ _printf_.c
@@ -2,73 +2,99 @@
 #include <unistd.h>
 
 /**
-* _printf - Produces output according to a format.
-* @format: The format string containing directives.
+*_print_char - Helper function to print a character.
 *
-* Return: The number of characters printed
+*@c: character to be printed
+*Return: inputted Character
 */
+static int _print_char(char c)
+{
+	return (write(1, &c, 1));
+}
 
+/**
+* _print_integer - Helper function to print an integer.
+*@num: number to be printed
+*Return: inputted interger
+*/
+static int _print_integer(int num)
+{
+	char buffer[20];
+	int i = 0;
+
+	if (num == 0)
+	{
+		buffer[i++] = '0';
+	}
+	else
+	{
+		if (num < 0)
+		{
+			write(1, "-", 1);
+			num = -num;
+		}
+
+		while (num > 0)
+		{
+			buffer[i++] = (num % 10) + '0';
+			num /= 10;
+		}
+	}
+
+	while (i > 0)
+	{
+		i--;
+		write(1, &buffer[i], 1);
+	}
+
+	return (i);
+}
+
+/**
+*_printf - Produces output according to a format.
+*@format : output format
+*Return: printed character
+*/
 int _printf(const char *format, ...)
 {
-va_list args;
-va_start(args, format);
+	int chara_print = 0;
+	va_list list;
 
-int chars_written = 0;
+	if (format == NULL)
+		return (-1);
 
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-{
+	va_start(list, format);
 
-write(STDOUT_FILENO, &c, 1);
-chars_written++;
-}
-break;
-case 's':
-{
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			chara_print += _print_char(*format);
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
 
-char *str = va_arg(args, char *);
-int str_length = 0;
-while (str[str_length] != '\0')
-{
-str_length++;
-}
-write(STDOUT_FILENO, str, str_length);
-chars_written += str_length;
-}
-break;
-case '%':
-{
+			if (*format == '%')
+			{
+				chara_print += _print_char(*format);
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(list, int);
 
-write(STDOUT_FILENO, "%", 1);
-chars_written++;
-}
-break;
-default:
-{
+				chara_print += _print_integer(num);
 
-write(STDOUT_FILENO, "%", 1);
-write(STDOUT_FILENO, format, 1);
-chars_written += 2;
-}
-break;
-}
-}
-else
-{
+			}
+		}
 
-write(STDOUT_FILENO, format, 1);
-chars_written++;
-}
-format++;
+		format++;
+	}
+
+	va_end(list);
+
+	return (chara_print);
 }
 
-va_end(args);
-
-return (chars_written);
-}
--- dI_specifiers.c
+++ dI_specifiers.c
@@ -2,44 +2,28 @@
 #include <unistd.h>
 
 /**
-* _printf - Produces output according to a format.
-* @format: The format string containing directives.
-*
-* Return: The number of characters printed.
+*print_char - Helper function to print a character.
+*@chara_print: location of character
+*@c: character to be printed
+*Return: printed character
 */
-int _printf(const char *format, ...)
+static void print_char(char c, int *chara_print)
 {
-int chara_print = 0;
-va_list list;
-
-if (format == NULL)
-return (-1);
-va_start(list, format);
-
-while (*format)
-{
-if (*format != '%')
-{
-write(1, format, 1);
-chara_print++;
+write(1, &c, 1);
+(*chara_print)++;
 }
-else
-{
-format++;
-if (*format == '\0')
-break;
 
-if (*format == '%')
+/**
+*print_integer - Helper function to print an integer.
+*@chara_print: location of character
+*@num: interger to be printed
+*Return: printed interger
+*/
+static void print_integer(int num, int *chara_print)
 {
-write(1, format, 1);
-chara_print++;
-}
-else if (*format == 'd' || *format == 'i')
-{
-int num = va_arg(list, int);
 char buffer[20];
-
 int i = 0;
+
 if (num == 0)
 {
 buffer[i++] = '0';
@@ -49,10 +33,9 @@ else
 if (num < 0)
 {
 write(1, "-", 1);
-chara_print++;
+(*chara_print)++;
 num = -num;
 }
-
 
 while (num > 0)
 {
@@ -61,13 +44,49 @@ num /= 10;
 }
 }
 
-
 while (i > 0)
 {
 i--;
 write(1, &buffer[i], 1);
-chara_print++;
+(*chara_print)++;
 }
+}
+
+/**
+*_printf - Produces output according to a format.
+*@format: output
+*Return: format of output
+*/
+int _printf(const char *format, ...)
+{
+int chara_print = 0;
+va_list list;
+
+if (format == NULL)
+return (-1);
+
+va_start(list, format);
+
+while (*format)
+{
+if (*format != '%')
+{
+print_char(*format, &chara_print);
+}
+else
+{
+format++;
+if (*format == '\0')
+break;
+
+if (*format == '%')
+{
+print_char(*format, &chara_print);
+}
+else if (*format == 'd' || *format == 'i')
+{
+int num = va_arg(list, int);
+print_integer(num, &chara_print);
 }
 }
 
